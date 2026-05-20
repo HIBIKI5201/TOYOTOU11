14bdbd328c2cfe9381f85bc1f42b3bdcc9379373
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 06:13:32 +0900

    [update]イベント追加

--- Assets/Scripts/Runtime/Title/ReadyButton.cs
+++ Assets/Scripts/Runtime/Title/ReadyButton.cs
@@ -1,12 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace TOYOTOU.Runtime
 {
     public class ReadyButton : MonoBehaviour
     {
+        public event Action<bool> OnValueChanged;
         public bool IsReady => _ready;
 
-        public void TriggeredReady() => _ready = !_ready;
+        public void TriggeredReady()
+        {
+            _ready = !_ready;
+            OnValueChanged?.Invoke(_ready);
+        }
 
         private bool _ready;
     }
