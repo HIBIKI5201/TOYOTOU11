f6093ee76477ec2c4869114d02c2e79b09854043
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 05:44:10 +0900

    [update]矢印UIを配列で処理

--- Assets/Scripts/Runtime/Title/UINavigater.cs
+++ Assets/Scripts/Runtime/Title/UINavigater.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.WSA;
 
 namespace TOYOTOU.Runtime
 {
@@ -13,6 +14,12 @@ namespace TOYOTOU.Runtime
             key.MoveAction.started += MoveHandler;
             key.Skill1Action.started += SkillHandler;
             key.Skill2Action.started += SkillHandler;
+
+            _selectArrows = new (_playerArrowLeft, _playerArrowRight,
+                _skill1ArrowLeft, _skill1ArrowRight,
+                _skill2ArrowLeft, _skill2ArrowRight);
+            _selectArrows.AllDeactive();
+            _selectArrows.Activate(_index);
         }
 
         public void Disable()
@@ -24,11 +31,21 @@ namespace TOYOTOU.Runtime
         }
 
         [SerializeField] private PlayerModelSelector _playerSelector;
+        [SerializeField] private UIArrow _playerArrowLeft;
+        [SerializeField] private UIArrow _playerArrowRight;
+        [Space]
         [SerializeField] private SkillSelector _skill1Selector;
+        [SerializeField] private UIArrow _skill1ArrowLeft;
+        [SerializeField] private UIArrow _skill1ArrowRight;
+        [Space]
         [SerializeField] private SkillSelector _skill2Selector;
+        [SerializeField] private UIArrow _skill2ArrowLeft;
+        [SerializeField] private UIArrow _skill2ArrowRight;
+        [Space]
         [SerializeField] private ReadyButton _ready;
 
         private InputActionKeyConfig _key;
+        private SelectArrows _selectArrows;
 
         private int _index;
 
@@ -37,19 +54,65 @@ namespace TOYOTOU.Runtime
             Vector2 input = context.ReadValue<Vector2>();
             if (input.y != 0)
             {
-                _index = Math.Sign(input.y) + _index;
+                int lastIndex = _index;
+                _index = Mathf.Clamp(Math.Sign(-input.y) + _index, 0, 2); // 下に行くほどインデックスが上がるため、input.yが逆転する。
+                _selectArrows.Deactivate(lastIndex);
+                _selectArrows.Activate(_index);
                 return;
             }
 
-            switch (_index)
+            if (input.x != 0)
             {
-                case 0: _playerSelector.Next(input.x); break;
+                switch (_index)
+                {
+                    case 0:
+                        _playerSelector.Next(input.x);
+                        break;
+                }
+
+                UIArrow arrow = input.x > 0 ? _selectArrows.GetRightArrow(_index) : _selectArrows.GetLeftArrow(_index);
+                arrow.Selected();
             }
         }
 
         private void SkillHandler(InputAction.CallbackContext context)
         {
             _ready.TriggeredReady();
+        }
+
+        private struct SelectArrows
+        {
+            public SelectArrows(params UIArrow[] arrows) => _arrows = arrows;
+
+            public void Activate(int index)
+            {
+                UIArrow left = GetLeftArrow(index);
+                UIArrow right = GetRightArrow(index);
+                left.gameObject.SetActive(true);
+                right.gameObject.SetActive(true);
+            }
+
+            public void Deactivate(int index)
+            {
+                UIArrow left = GetLeftArrow(index);
+                UIArrow right = GetRightArrow(index);
+                left.gameObject.SetActive(false);
+                right.gameObject.SetActive(false);
+            }
+
+            public void AllDeactive()
+            {
+                int n = _arrows.Length / 2;
+                for (int i = 0; i < n; i++)
+                {
+                    Deactivate(i);
+                }
+            }
+
+            public UIArrow GetLeftArrow(int index) => _arrows[index * 2 + 1];
+            public UIArrow GetRightArrow(int index) => _arrows[index * 2];
+
+            private UIArrow[] _arrows;
         }
     }
 }
