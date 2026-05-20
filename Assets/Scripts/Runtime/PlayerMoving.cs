b179947ffaf773499c255df0d83a2dc33482550f
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-20 20:55:17 +0900

    [update]名前空間を最適化

--- Assets/Scripts/Runtime/PlayerMoving.cs
+++ Assets/Scripts/Runtime/PlayerMoving.cs
@@ -1,114 +1,115 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 
-public class PlayerMoving : MonoBehaviour
+namespace TOYOTOU.Runtime
 {
-    [SerializeField] public int Hitpoint = 10;
-    [SerializeField] public int Attack = 10;
-    [SerializeField] public float bounceForce = 10f;
-
-    [Header("加速力")]
-    [SerializeField] private float _addSpeed = 0.5f;
-
-    [Header("最大速度")]
-    [SerializeField, Tooltip("最大速度")] private Vector3 _maxSpeed;
-
-    [SerializeField] private Vector3 _startSpeed;
-
-    private Rigidbody _rb;
-    private Vector3 _moveDirection;
-    private Vector3 _addVelocity;
-    private Vector3 _currentMoveVelocity;
-
-    public GameObject Bey;
-
-    private void Awake()
+    public class PlayerMoving : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody>();
-    }
+        [SerializeField] public int Hitpoint = 10;
+        [SerializeField] public int Attack = 10;
+        [SerializeField] public float bounceForce = 10f;
 
-    private void Start()
-    {
-        _currentMoveVelocity = new Vector3(_startSpeed.x, 0, _startSpeed.z);
-    }
+        [Header("加速力")]
+        [SerializeField] private float _addSpeed = 0.5f;
 
-    private void Update()
-    {
-        float x = 0;
-        float z = 0;
+        [Header("最大速度")]
+        [SerializeField, Tooltip("最大速度")] private Vector3 _maxSpeed;
 
-        Keyboard keyboard = Keyboard.current;
+        [SerializeField] private Vector3 _startSpeed;
 
-        if (keyboard == null) return;
+        private Rigidbody _rb;
+        private Vector3 _moveDirection;
+        private Vector3 _addVelocity;
+        private Vector3 _currentMoveVelocity;
 
-        var wKey = keyboard.wKey;
-        var aKey = keyboard.aKey;
-        var dKey = keyboard.dKey;
-        var sKey = keyboard.sKey;
+        public GameObject Bey;
 
-        if (wKey.IsPressed())// 前s
+        private void Awake()
         {
-            x = 1;
-        }
-        else if (sKey.IsPressed()) // 後ろ
-        {
-            x = -1;
+            _rb = GetComponent<Rigidbody>();
         }
 
-        if (dKey.IsPressed())// 右
+        private void Start()
         {
-            z = 1;
-        }
-        else if (aKey.IsPressed())// 左
-        {
-            z = -1;
+            _currentMoveVelocity = new Vector3(_startSpeed.x, 0, _startSpeed.z);
         }
 
-        _moveDirection = new Vector3(x, 0, z);
-        _addVelocity = _moveDirection * _addSpeed;
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (gameObject.CompareTag("Player"))
+        private void Update()
         {
-            PlayerMoving PlayerRotation = collision.gameObject.GetComponent<PlayerMoving>();
-            if (PlayerRotation != null)
+            float x = 0;
+            float z = 0;
+
+            Keyboard keyboard = Keyboard.current;
+
+            if (keyboard == null) return;
+
+            var wKey = keyboard.wKey;
+            var aKey = keyboard.aKey;
+            var dKey = keyboard.dKey;
+            var sKey = keyboard.sKey;
+
+            if (wKey.IsPressed())// 前s
             {
-                PlayerRotation.TakeDamage(Attack);
-                Rigidbody otherRb=collision.gameObject.GetComponent<Rigidbody>();
-                if (otherRb != null) 
-                {
-                ContactPoint contact=collision.contacts[0];
-                Vector3 pushDirection = contact.normal;
-                GetComponent<Rigidbody>().AddForce(pushDirection*bounceForce, ForceMode.Impulse);
-                otherRb.AddForce(pushDirection*bounceForce,ForceMode.Impulse);
+                x = 1;
+            }
+            else if (sKey.IsPressed()) // 後ろ
+            {
+                x = -1;
+            }
 
+            if (dKey.IsPressed())// 右
+            {
+                z = 1;
+            }
+            else if (aKey.IsPressed())// 左
+            {
+                z = -1;
+            }
+
+            _moveDirection = new Vector3(x, 0, z);
+            _addVelocity = _moveDirection * _addSpeed;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (gameObject.CompareTag("Player"))
+            {
+                PlayerMoving PlayerRotation = collision.gameObject.GetComponent<PlayerMoving>();
+                if (PlayerRotation != null)
+                {
+                    PlayerRotation.TakeDamage(Attack);
+                    Rigidbody otherRb = collision.gameObject.GetComponent<Rigidbody>();
+                    if (otherRb != null)
+                    {
+                        ContactPoint contact = collision.contacts[0];
+                        Vector3 pushDirection = contact.normal;
+                        GetComponent<Rigidbody>().AddForce(pushDirection * bounceForce, ForceMode.Impulse);
+                        otherRb.AddForce(pushDirection * bounceForce, ForceMode.Impulse);
+
+                    }
                 }
             }
         }
-    }
 
-    public void TakeDamage(int Attack)
-    {
-     Hitpoint -= Attack;
-        if(Hitpoint < 0)
+        public void TakeDamage(int Attack)
         {
-            Destroy(this);
+            Hitpoint -= Attack;
+            if (Hitpoint < 0)
+            {
+                Destroy(this);
+            }
         }
-     }
-   
-    private void Move()
-    {
-        _currentMoveVelocity += _addVelocity;
-        _rb.linearVelocity = new Vector3(_currentMoveVelocity.x, _rb.linearVelocity.y, _currentMoveVelocity.z);
+
+        private void Move()
+        {
+            _currentMoveVelocity += _addVelocity;
+            _rb.linearVelocity = new Vector3(_currentMoveVelocity.x, _rb.linearVelocity.y, _currentMoveVelocity.z);
+        }
     }
 }
