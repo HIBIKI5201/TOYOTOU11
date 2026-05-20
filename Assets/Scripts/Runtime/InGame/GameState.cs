d08dc995b2092da322588e6b4daebd694221498d
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 04:29:49 +0900

    [fix]ステートの保持するモデルをコントローラークラスに

--- Assets/Scripts/Runtime/InGame/GameState.cs
+++ Assets/Scripts/Runtime/InGame/GameState.cs
@@ -4,8 +4,8 @@ namespace TOYOTOU.Runtime
 {
     public class GameState : MonoBehaviour
     {
-        public GameObject Player1Model;
-        public GameObject Player2Model;
+        public PlayerModelController Player1Model;
+        public PlayerModelController Player2Model;
 
         public PlayerStatus Player1Status;
         public PlayerStatus Player2Status;
