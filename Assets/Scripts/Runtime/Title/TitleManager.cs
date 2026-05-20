3a512c94752b344acba247d4c4997744df8e1c8b
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 04:31:29 +0900

    [update]セレクターから選択モデルを保持

--- Assets/Scripts/Runtime/Title/TitleManager.cs
+++ Assets/Scripts/Runtime/Title/TitleManager.cs
@@ -7,17 +7,18 @@ namespace TOYOTOU.Runtime
         [SerializeField] private GameManager _gm;
         [SerializeField] private InGameLoopManager _ingameLoop;
 
+        [SerializeField] private PlayerModelSelector _selector1;
+        [SerializeField] private PlayerModelSelector _selector2;
+
         [ContextMenu(nameof(GameStart))]
         public void GameStart()
         {
+            _gm.State.Player1Model = _selector1.GetSelectedModel();
+            _gm.State.Player2Model = _selector2.GetSelectedModel();
+
             _gm.SetActiveIngameObjs(true);
             _ingameLoop.GameStart();
             _gm.SetActiveTitleObjs(false);
-        }
-
-        private void OnEnable()
-        {
-            
         }
     }
 }
