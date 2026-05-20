fa259352fc0809647bd6a0756a6a7b575a63865d
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 03:03:40 +0900

    [update]手動でゲームを始められるように

--- Assets/Scripts/Runtime/Title/TitleManager.cs
+++ Assets/Scripts/Runtime/Title/TitleManager.cs
@@ -7,11 +7,17 @@ namespace TOYOTOU.Runtime
         [SerializeField] private GameManager _gm;
         [SerializeField] private InGameLoopManager _ingameLoop;
 
+        [ContextMenu(nameof(GameStart))]
         public void GameStart()
         {
             _gm.SetActiveIngameObjs(true);
             _ingameLoop.GameStart();
             _gm.SetActiveTitleObjs(false);
+        }
+
+        private void OnEnable()
+        {
+            
         }
     }
 }
