f8ca9b05553f611e56e072d413cd77bbf3859046
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 04:30:41 +0900

    [update]選択中のモデルを返すメソッド追加

--- Assets/Scripts/Runtime/Title/PlayerModelSelector.cs
+++ Assets/Scripts/Runtime/Title/PlayerModelSelector.cs
@@ -4,27 +4,29 @@ namespace TOYOTOU.Runtime
 {
     public class PlayerModelSelector : MonoBehaviour
     {
+        public PlayerModelController GetSelectedModel() => _characterModels[_index];
+
         public void Change(int index)
         {
             index = (index + _characterModels.Length) % _characterModels.Length;
 
-            CharacterModelController model = _characterModels[index];
+            PlayerModelController model = _characterModels[index];
             ActivateModel(model);
             DeactivateModel(_characterModels[_index]);
             _index = index;
         }
 
         [SerializeField] private PlayerStatus[] _stetuses;
-        private CharacterModelController[] _characterModels;
+        private PlayerModelController[] _characterModels;
 
         private int _index;
 
         private void Start()
         {
-            _characterModels = new CharacterModelController[_stetuses.Length];
+            _characterModels = new PlayerModelController[_stetuses.Length];
             for (int i = 0; i < _stetuses.Length; i++)
             {
-                CharacterModelController model = Instantiate(_stetuses[i].Model, transform);
+                PlayerModelController model = Instantiate(_stetuses[i].Model, transform);
                 DeactivateModel(model);
                 _characterModels[i] = model;
             }
@@ -32,12 +34,12 @@ namespace TOYOTOU.Runtime
             ActivateModel(_characterModels[0]);
         }
 
-        private void ActivateModel(CharacterModelController model)
+        private void ActivateModel(PlayerModelController model)
         {
             model.gameObject.SetActive(true);
         }
 
-        private void DeactivateModel(CharacterModelController model)
+        private void DeactivateModel(PlayerModelController model)
         {
             model.gameObject.SetActive(false);
         }
