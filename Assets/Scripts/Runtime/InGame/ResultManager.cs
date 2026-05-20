eb7fac2d84e2a2775b68622e3608b6279522c8c5
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 01:42:17 +0900

    [update]リザルトでゲーム終了イベントを追加

--- Assets/Scripts/Runtime/InGame/ResultManager.cs
+++ Assets/Scripts/Runtime/InGame/ResultManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TOYOTOU.Runtime;
 using UnityEngine;
 
@@ -6,6 +8,8 @@ namespace TOYOTOU
 {
     public class ResultManager : MonoBehaviour
     {
+        public event Action OnGameEndTriggered;
+
         public void JudgeWinner(PlayerInitializer playerInitializer)
         {
             PlayerManager player1 = playerInitializer.Player1;
@@ -54,6 +58,22 @@ namespace TOYOTOU
         public void SetActivePlayer1UI() => _player1WinUI.ForEach(go => go.SetActive(true));
         public void SetActivePlayer2UI() => _player2WinUI.ForEach(go => go.SetActive(true));
         public void SetActiveDrawUI() => _drawUI.ForEach(go => go.SetActive(true));
+
+        public async ValueTask WaitPressedGameEnd()
+        {
+            TaskCompletionSource<byte> task = new();
+
+            Action onTriggerdAction = null;
+            onTriggerdAction = () =>
+            {
+                OnGameEndTriggered -= onTriggerdAction;
+                task.SetResult(0);
+            };
+
+            OnGameEndTriggered += onTriggerdAction;
+
+            await task.Task;
+        }
 
         [SerializeField] private List<GameObject> _player1WinUI;
         [SerializeField] private List<GameObject> _player2WinUI;
