b8958227ea8981ff756be562f77f2df60695dc99
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 04:09:36 +0900

    [update]ゲームのリセットを追加

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -1,5 +1,7 @@
+using SymphonyFrameWork.Attribute;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace TOYOTOU.Runtime
 {
@@ -7,9 +9,11 @@ namespace TOYOTOU.Runtime
     {
         public void SetActiveTitleObjs(bool value) => _titleObjes.ForEach(go => go.SetActive(value));
         public void SetActiveIngameObjs(bool value) => _ingameObjs.ForEach(go => go.SetActive(value));
+        public void GameReset() => SceneManager.LoadScene(_sceneName);
 
         [SerializeField] List<GameObject> _titleObjes;
         [SerializeField] List<GameObject> _ingameObjs;
+        [SerializeField, SceneNameSelector] string _sceneName;
 
         private void Start()
         {
