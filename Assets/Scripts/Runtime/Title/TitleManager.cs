a489191a9266e655f98578106185ab6f03fc0f04
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 05:13:29 +0900

    [update]タイトルのマネージャークラスを追加

--- Assets/Scripts/Runtime/Title/TitleManager.cs
+++ Assets/Scripts/Runtime/Title/TitleManager.cs
@@ -7,8 +7,14 @@ namespace TOYOTOU.Runtime
         [SerializeField] private GameManager _gm;
         [SerializeField] private InGameLoopManager _ingameLoop;
 
+        [SerializeField] private InputActionKeyConfig _player1;
+        [SerializeField] private InputActionKeyConfig _player2;
+
         [SerializeField] private PlayerModelSelector _selector1;
         [SerializeField] private PlayerModelSelector _selector2;
+
+        [SerializeField] private UINavigater _player1Navigater;
+        [SerializeField] private UINavigater _player2Navigater;
 
         [ContextMenu(nameof(GameStart))]
         public void GameStart()
@@ -19,6 +25,18 @@ namespace TOYOTOU.Runtime
             _gm.SetActiveIngameObjs(true);
             _ingameLoop.GameStart();
             _gm.SetActiveTitleObjs(false);
+        }
+
+        private void OnEnable()
+        {
+            _player1Navigater.Enable(_player1);
+            _player2Navigater.Enable(_player2);
+        }
+
+        private void OnDisable()
+        {
+            _player1Navigater.Disable();
+            _player2Navigater.Disable();
         }
     }
 }
