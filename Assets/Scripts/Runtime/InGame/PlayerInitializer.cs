534f1888433bcbd563d81881dad91fa2cf3590c6
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-20 21:50:16 +0900

    [add]プレイヤーの初期化クラスを追加

---
+++ Assets/Scripts/Runtime/InGame/PlayerInitializer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace TOYOTOU.Runtime
+{
+    public class PlayerInitializer : MonoBehaviour
+    {
+        public PlayerStatus _player1Status;
+        public PlayerStatus _player2Status;
+
+        public void Init()
+        {
+            _player1Manager.Init(_player1Status);
+            _player2Manager.Init(_player2Status);
+        }
+
+        [SerializeField] private PlayerManager _player1Manager;
+        [SerializeField] private PlayerManager _player2Manager;
+    }
+}
