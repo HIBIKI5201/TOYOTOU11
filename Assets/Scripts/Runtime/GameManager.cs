9d144b1aae6120ac9dd8a56ce400ea771ae82522
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 02:38:48 +0900

    [update]ゲームの初期実行が行えるようにエントリーポイントを実装

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -5,10 +5,16 @@ namespace TOYOTOU.Runtime
 {
     public class GameManager : MonoBehaviour
     {
-        public void SetActiveTitleObjs() => _titleObjes.ForEach(go => go.SetActive(true));
-        public void SetActiveIngameObjs() => _ingameObjs.ForEach(go => go.SetActive(true));
+        public void SetActiveTitleObjs(bool value) => _titleObjes.ForEach(go => go.SetActive(value));
+        public void SetActiveIngameObjs(bool value) => _ingameObjs.ForEach(go => go.SetActive(value));
 
         [SerializeField] List<GameObject> _titleObjes;
         [SerializeField] List<GameObject> _ingameObjs;
+
+        private void Start()
+        {
+            SetActiveTitleObjs(true);
+            SetActiveIngameObjs(false);
+        }
     }
 }
