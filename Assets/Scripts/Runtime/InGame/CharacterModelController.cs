5f9e99d319a134f0668f7c3113e5fba42705d474
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 04:09:00 +0900

    [add]キャラクターのモデルコントローラーを追加

---
+++ Assets/Scripts/Runtime/InGame/CharacterModelController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace TOYOTOU
+{
+    public class CharacterModelController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
