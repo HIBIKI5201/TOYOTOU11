01c1b2188b179ea345ce55a3814b63b1af94bf78
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 01:35:55 +0900

    [update]リザルト計算を追加

--- Assets/Scripts/Runtime/InGame/ResultManager.cs
+++ Assets/Scripts/Runtime/InGame/ResultManager.cs
@@ -1,21 +1,70 @@
 using System.Collections.Generic;
+using TOYOTOU.Runtime;
 using UnityEngine;
 
 namespace TOYOTOU
 {
     public class ResultManager : MonoBehaviour
     {
+        public void JudgeWinner(PlayerInitializer playerInitializer)
+        {
+            PlayerManager player1 = playerInitializer.Player1;
+            PlayerManager player2 = playerInitializer.Player2;
+            float player1HP = player1.RemainHitPoint;
+            float player2HP = player2.RemainHitPoint;
+
+            bool isPlayer1Dead = player1HP <= 0;
+            bool isPlayer2Dead = player2HP <= 0;
+
+            if (isPlayer1Dead && isPlayer2Dead)
+            {
+                SetActiveDrawUI();
+                return;
+            }
+
+            if (isPlayer1Dead)
+            {
+                SetActivePlayer2UI();
+                return;
+            }
+
+            if (isPlayer2Dead)
+            {
+                SetActivePlayer1UI();
+                return;
+            }
+
+            if (player1HP == player2HP)
+            {
+                SetActiveDrawUI();
+            }
+            else
+            {
+                if (player1HP > player2HP)
+                {
+                    SetActivePlayer1UI();
+                }
+                else
+                {
+                    SetActivePlayer2UI();
+                }
+            }
+        }
+
         public void SetActivePlayer1UI() => _player1WinUI.ForEach(go => go.SetActive(true));
         public void SetActivePlayer2UI() => _player2WinUI.ForEach(go => go.SetActive(true));
+        public void SetActiveDrawUI() => _drawUI.ForEach(go => go.SetActive(true));
 
         [SerializeField] private List<GameObject> _player1WinUI;
         [SerializeField] private List<GameObject> _player2WinUI;
+        [SerializeField] private List<GameObject> _drawUI;
 
         [ContextMenu("Inactive UI")]
         private void Awake()
         {
             _player1WinUI.ForEach(go => go.SetActive(false));
             _player2WinUI.ForEach(go => go.SetActive(false));
+            _drawUI.ForEach(go => go.SetActive(false));
         }
     }
 }
