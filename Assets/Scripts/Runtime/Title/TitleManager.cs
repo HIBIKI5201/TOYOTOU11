a0ed908f9ea574c9a40cc6755c74ea17b679be20
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 02:39:04 +0900

    [update]ゲーム開始時の処理を実装

--- Assets/Scripts/Runtime/Title/TitleManager.cs
+++ Assets/Scripts/Runtime/Title/TitleManager.cs
@@ -4,6 +4,14 @@ namespace TOYOTOU.Runtime
 {
     public class TitleManager : MonoBehaviour
     {
+        [SerializeField] private GameManager _gm;
+        [SerializeField] private InGameLoopManager _ingameLoop;
 
+        public void GameStart()
+        {
+            _gm.SetActiveIngameObjs(true);
+            _ingameLoop.GameStart();
+            _gm.SetActiveTitleObjs(false);
+        }
     }
 }
