2c3e58e433b118839db06c51531aadb3a7437a8b
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 07:08:11 +0900

    [update]矢印UIも動くように

--- Assets/Scripts/Runtime/Title/UIArrow.cs
+++ Assets/Scripts/Runtime/Title/UIArrow.cs
@@ -1,12 +1,16 @@
+using DG.Tweening;
 using UnityEngine;
 
-namespace TOYOTOU
+namespace TOYOTOU.Runtime
 {
     public class UIArrow : MonoBehaviour
     {
         public void Selected()
         {
-
+            DOTween.Sequence()
+                .Append(transform.DOScale(0.6f, 0.1f))
+                .Append(transform.DOScale(1f, 0.1f))
+                .SetLink(gameObject);
         }
     }
 }
