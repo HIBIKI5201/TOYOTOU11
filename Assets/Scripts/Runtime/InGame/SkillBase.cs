6b9a96b93862fc297d48f9f95f0e4357a4cc139e
HIBIKI_5201 <takuto5201@gmail.com>
2026-05-21 12:37:35 +0900

    [update]クールタイム

--- Assets/Scripts/Runtime/InGame/SkillBase.cs
+++ Assets/Scripts/Runtime/InGame/SkillBase.cs
@@ -1,9 +1,14 @@
+using TOYOTOU.Runtime;
 using UnityEngine;
 
 namespace TOYOTOU
 {
     public abstract class SkillBase
     {
+        public float CoolTime => _cooltime;
+
+        public abstract void Execute(PlayerManager self, PlayerManager other);
+
         [SerializeField]
         protected float _cooltime;
     }
